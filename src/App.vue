ce807d9a1fe5fcd8a5d8a241e80d507a7a14279b
YoshizakiKota <yoshizaki@genio.co.jp>
2019-03-26 16:51:40 +0900

    style

--- src/App.vue
+++ src/App.vue
@@ -103,5 +103,17 @@ header ul li.current a {
     position: relative;
     left: -50%;
 }
+
+.contents ul, ol {
+    background: #fffcf4;
+    border-radius :8px;
+    box-shadow :0px 0px 5px silver;
+    padding: 0.5em 0.5em 0.5em 2em;
+}
+
+.contents ul li, ol li {
+    line-height: 1.5;
+    padding: 0.5em 0;
+}
 </style>
 
